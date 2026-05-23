5b34de96d7f014345f383d6a02b45a2721f91ec8
Nguyen Truong An (K18 Hl) <annthe186612@fpt.edu.vn>
2026-05-23 16:22:01 +0700

    hehehe

--- sepay_test/script.js
+++ sepay_test/script.js
@@ -10,16 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // CẤU HÌNH NGÂN HÀNG CỦA BẠN TẠI ĐÂY
     // Dùng API của VietQR (vietqr.io)
     const BANK_ID = "MB"; // Thay bằng mã ngân hàng của bạn (VD: VCB, TCBM, MB, BIDV)
-    const ACCOUNT_NO = "0123456789"; // Thay bằng Số tài khoản ngân hàng của bạn
-    const ACCOUNT_NAME = "NGUYEN VAN A"; // Tên chủ tài khoản
-    const AMOUNT = 2500000; // Số tiền (2,500,000 VNĐ)
+    const ACCOUNT_NO = "0927319622"; // Thay bằng Số tài khoản ngân hàng của bạn
+    const ACCOUNT_NAME = "NGUYEN TRUONG AN"; // Tên chủ tài khoản
+    const AMOUNT = 2000; // Số tiền (2,500,000 VNĐ)
     const SEPAY_PREFIX = "DH"; // Tiền tố mã đơn hàng (giống cấu hình trên SePay)
 
     buyButton.addEventListener('click', () => {
         // Tạo mã đơn hàng ngẫu nhiên, ví dụ: DH58392
         const randomOrderCode = Math.floor(10000 + Math.random() * 90000);
         const orderId = `${SEPAY_PREFIX}${randomOrderCode}`;
-        
+
         // Hiển thị mã đơn hàng lên UI
         orderIdDisplay.textContent = orderId;
 
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Format URL sinh mã QR của VietQR
         const qrUrl = `https://img.vietqr.io/image/${BANK_ID}-${ACCOUNT_NO}-compact2.jpg?amount=${AMOUNT}&addInfo=${transferContent}&accountName=${encodeURIComponent(ACCOUNT_NAME)}`;
-        
+
         // Load QR Code
         qrImage.src = qrUrl;
         qrImage.onload = () => {
